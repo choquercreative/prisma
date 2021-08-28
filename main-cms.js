742fe22a9e70a22261f53f80897e8cad33225130
choquercreative <89684714+choquercreative@users.noreply.github.com>
2021-08-28 02:08:19 -0700

    Create main-cms.js

---
+++ main-cms.js
@@ -0,0 +1,45 @@
+// Remove Empty CMS
+if ( $('.cms-filter_item.series').length < 1 ) {
+	$('.filter-contain.series').remove();
+}
+
+
+
+// Remove Empty CMS
+if ( $('.cms-filter_item.set').length < 1 ) {
+	$('.filter-contain.set').remove();
+}
+
+
+
+// Remove Empty CMS
+if ( $('.cms-filter_item.product-type').length < 1 ) {
+	$('.filter-contain.product-type').remove();
+}
+
+
+
+// Remove Empty CMS
+if ( $('.cms-filter_item.card-type').length < 1 ) {
+	$('.filter-contain.card-type').remove();
+}
+
+
+// Remove Empty CMS
+if ( $('.cms-filter_item.card-color').length < 1 ) {
+	$('.filter-contain.card-color').remove();
+}
+
+
+
+// Remove Empty CMS
+if ( $('.cms-filter_item.rarity').length < 1 ) {
+	$('.filter-contain.rarity').remove();
+}
+
+
+
+// Remove Empty CMS
+if ( $('.cms-filter_item.card-finish').length < 1 ) {
+	$('.filter-contain.card-finish').remove();
+}
